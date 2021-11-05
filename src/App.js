e2ce4dcb794c8d3b4c2a86ca337250e772408a36
TylerRWiddison <tyler.rwiddison@gmail.com>
2021-11-05 14:20:55 -0600

    added dummy data and logic to view user places

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 import "./App.css";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import UserPlaces from "./places/pages/UserPlaces";
 import { Fragment } from "react/cjs/react.production.min";
 
 const App = () => {
@@ -20,6 +21,9 @@ const App = () => {
           <Switch>
             <Route path="/" exact>
               <Users />
+            </Route>
+            <Route path='/:userId/places' exact>
+              <UserPlaces />
             </Route>
             <Route path="/places/new" exact>
               <NewPlace />
---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,39 @@
+import { useParams } from "react-router";
+
+import PlaceList from "../components/PlaceList";
+
+
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Empire State Building',
+        description: 'One of the most famous sky scrapers in the world!',
+        imageUrl:  'https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg',
+        address: '20 W 34th St, New York, NY 10001, United States',
+        location: {
+            lat: 40.7484405,
+            lng: -73.9878584
+        },
+        creator: 'u1'
+    },
+    {
+        id: 'p2',
+        title: 'Trier',
+        description: 'Home to the black gate!',
+        imageUrl:  'https://upload.wikimedia.org/wikipedia/commons/b/b1/Trier-Blick-vom_Weishaus.JPG',
+        address: 'Rhineland-Palatinate, Germany',
+        location: {
+            lat: 49.7500,
+            lng: 6.6371
+        },
+        creator: 'u2'
+    }
+]
+
+const UserPlaces = () => {
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId)
+  return <PlaceList items={loadedPlaces} />;
+};
+
+export default UserPlaces;
