4f47bfb688ddc8f31e9a0ff0e25a8099cb100201
TylerRWiddison <tyler.rwiddison@gmail.com>
2021-11-15 14:03:24 -0700

    Added fileupload for places and delete image file when place is deleted

--- Frontend/src/user/pages/Auth.js
+++ Frontend/src/user/pages/Auth.js
@@ -85,13 +85,13 @@ const Auth = () => {
     } else {
       try {
         const formData = new FormData();
-        formData.append('email', formState.inputs.email.value);
-        formData.append('name', formState.inputs.name.value);
-        formData.append('password', formState.inputs.password.value);
-        formData.append('image', formState.inputs.image.value);
+        formData.append("email", formState.inputs.email.value);
+        formData.append("name", formState.inputs.name.value);
+        formData.append("password", formState.inputs.password.value);
+        formData.append("image", formState.inputs.image.value);
         const responseData = await sendRequest(
-          'http://localhost:5000/api/users/signup',
-          'POST',
+          "http://localhost:5000/api/users/signup",
+          "POST",
           formData
         );
 
@@ -120,7 +120,12 @@ const Auth = () => {
             />
           )}
           {!isLoginMode && (
-            <ImageUpload center id="image" onInput={inputHandler} />
+            <ImageUpload
+              center
+              id="image"
+              onInput={inputHandler}
+              errorText="Please provide an image."
+            />
           )}
           <Input
             element="input"
